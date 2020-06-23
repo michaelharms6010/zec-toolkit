3ed9bff25ec60d09c95e542230a012282dd9b380
Michael Harms <michael.harms6010@gmail.com>
2020-06-22 21:45:36 -0500

    adds some validation functions

---
+++ index.js
@@ -0,0 +1,15 @@
+const zaddrRegex = /^zs[a-z0-9]{76}$/;
+const taddrRegex = /^t1[AC-HJ-NP-Zac-hj-np-z02-9]{33}$/;
+
+
+exports.isValidZaddr = function(zaddr) {
+    return zaddrRegex.test(zaddr)
+}
+
+exports.isValidTaddr = function(taddr) {
+    return taddrRegex.test(taddr)
+}
+
+exports.isValidAddress = function(address) {
+    return taddrRegex.test(address) || zaddrRegex.test(address)
+}
